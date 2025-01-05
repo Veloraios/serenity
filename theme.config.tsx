aa649cc2907a90e85e80160f335b1c7790707be4
velora <serenityios41@gmail.com>
2025-01-05 14:42:54 +0100

    serenity

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Serenity</span>,
+  logo: (
+    <div>
+      <img
+        src="/serenity.jpg"
+        alt="Logo"
+        style={{
+          height: '40px',
+          borderRadius: '15px', // Add this line to set the border radius
+        }}
+      />
+      <span>Serenity</span>
+    </div>
+  ),
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
