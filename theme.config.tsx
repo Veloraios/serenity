965cc00726dc1b1ada87974218d0b7bb86431301
velora <serenityios41@gmail.com>
2025-01-06 09:16:48 +0100

    test2

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 import { useRouter } from 'next/router'
-import Head from 'next/head'
 
 const config: DocsThemeConfig = {
   logo: (
@@ -33,25 +32,16 @@ const config: DocsThemeConfig = {
   useNextSeoProps() {
     const { asPath } = useRouter()
     return {
-      titleTemplate: asPath === '/' ? '%s' : '%s', // Keeps title clean without "Nextra"
+      titleTemplate: asPath === '/' ? '%s' : '%s', // Remove "Nextra" from all pages
+      description: '', // Remove description meta tag
+      openGraph: {
+        description: '', // Remove OpenGraph description
+      },
+      twitter: {
+        description: '', // Remove Twitter description
+      },
     }
   },
-  // You can add Open Graph meta tags here to control how it looks on Discord.
-  Head: () => (
-    <>
-      <Head>
-        {/* Open Graph meta tags */}
-        <meta property="og:title" content="Serenity" />
-        <meta property="og:description" content="Welcome to Serenity Docs" />
-        <meta property="og:image" content="/serenity.jpg" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Serenity" />
-        <meta name="twitter:description" content="Welcome to Serenity Docs" />
-        <meta name="twitter:image" content="/serenity.jpg" />
-      </Head>
-    </>
-  ),
 }
 
 export default config
-
